c51398018fd36e19564aaad33f15d6b626f88954
kukli07 <84287747+kukli07@users.noreply.github.com>
2023-05-11 12:46:06 +0200

    Add better comments

--- easy/containsDuplicate.ts
+++ easy/containsDuplicate.ts
@@ -2,15 +2,16 @@ function containsDuplicate(nums: number[]): boolean {
     // Map to count the occurrence of each number in nums
     const occurrenceMap = {};
 
-    // Loop over nums and add it to the occurrenceMap.
+    // Loop over nums
     for (let i = 0; i < nums.length; i++) {
+        // If we saw the number: return true, else add it to the map
         if (occurrenceMap[nums[i]]) {
             return true;
         } else {
             occurrenceMap[nums[i]] = 1;
         }
     }
-
+    // Return false, if the loop did not "break"
     return false;
 }
 
